cf64c2bcb71b8ea986928a4ea6195da99f985451
xtzeng <xiaoti_zeng@126.com>
2015-03-23 02:22:23 +0800

    quartz_basicDemo

---
+++ src/main/java/com/ncs/hj/SpringQtz.java
@@ -0,0 +1,13 @@
+package com.ncs.hj;
+
+import java.util.Date;
+
+public class SpringQtz {
+	private static int counter = 0;
+	protected void execute()  {
+		long ms = System.currentTimeMillis();
+		System.out.println("\t\t" + new Date(ms));
+		System.out.println("(" + counter++ + ")");
+	}
+}
+
